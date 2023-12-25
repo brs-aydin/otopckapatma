863bd56f2e51e925ba5264cdef52f012177fbe7d
Barış Aydın <51105834+brs-aydin@users.noreply.github.com>
2023-12-25 17:14:00 +0300

    Otomatik PC Kapatma
    Kaynak Kodları

---
+++ OtoKapatma/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace OtoKapatma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.label1.Font = new System.Drawing.Font("Sugarpunch DEMO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(141, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "156";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.label2.Font = new System.Drawing.Font("Sugarpunch DEMO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(75, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kaç Saat Sonra Kapansın?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Sugarpunch DEMO", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(107, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "dakika sonra";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-36, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(381, 239);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(343, 322);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "                   OTOMATİK KAPATMA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ OtoKapatma/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtoKapatma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int gerisayim;//geri sayimi formda ki istediğim her bloğa kullanmak için int değerine tanıdım
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text!="")//Textbox boş değilse yapılacaklar 
+            {
+
+                int sayi = Convert.ToInt32(textBox1.Text);//sayı değerini textboxta ki yazılana eşitliyorum
+
+                gerisayim = sayi * 60;//Saat olarak hesapladığım ve timer intervalini dakika yaptığım için 60 ile çarpıyorum 
+                label1.Text = gerisayim.ToString();//labeli geri sayımdaki ilk sayıya eşitliyorum ki 1 dk sayı gelmesi beklenmesin
+               //burda da gizleme işlemi false olan gözükmüyor true olan gözüküyor
+                textBox1.Visible = false;
+                button1.Enabled = false;//buton kullanıma kapıyoruz.
+                label2.Visible = false;
+                label1.Visible = true;
+                label3.Visible = true;
+                pictureBox1.Visible = true;
+                timer1.Start();//Zamanlayıcı baslıyor
+            }
+          
+           
+        }
+
+   
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+                gerisayim--;//geri sayımda ki sayı düşüyor (intervale dikkat edin dakika için 60000 kullanıyoruz saniye için 1000)
+                label1.Text = gerisayim.ToString();//labeli otomatik düzeltmesi icin yine geri sayıma bağladık
+                if (gerisayim == 0)//geri sayım 0 i gösterdiğinde
+                {
+
+                    timer1.Stop();//timer duracak
+                    System.Diagnostics.Process.Start("shutdown", "-f -s");//kapatma kodu calısacak
+                
+                }
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //gizleme işlemleri
+            label1.Visible = false;
+            label3.Visible = false;
+            pictureBox1.Visible = false;
+        }
+
+    
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          //gizleme işlemleri
+            textBox1.Visible = true;
+            button1.Enabled = true;
+            label2.Visible = true;
+            label1.Visible = false;
+            label3.Visible = false;
+            pictureBox1.Visible = false;
+            textBox1.Clear();//textbox içinde ki kalan veriler silinecek
+            timer1.Stop();//timer duracak
+        }
+
+  
+
+      
+     
+        }
+    }
+
